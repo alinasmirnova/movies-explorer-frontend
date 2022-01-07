a503c9a75ab36a7b3d5281a400301b7e555d0302
Alina Smirnova <BlueOstrich@yandex.ru>
2022-01-07 19:56:57 +0300

    fix: footer section

--- movies-explorer/src/components/Header/Header.js
+++ movies-explorer/src/components/Header/Header.js
@@ -5,11 +5,11 @@ import logo from'../../images/logo.svg'
 
 function Header() {
     return (
-        <div className="header">
+        <section className="header">
             <img className="header__logo" src={logo} alt="Учебный проект" />
             <Navigation />
             <Auth className="header__auth" />
-        </div>
+        </section>
     );
 }
 
