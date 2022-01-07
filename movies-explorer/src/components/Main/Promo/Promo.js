4ff600790a1f2e13feb232cf1d48a3effc1d077f
Alina Smirnova <BlueOstrich@yandex.ru>
2022-01-07 22:31:01 +0300

    feat: адаптив под планшет

--- movies-explorer/src/components/Main/Promo/Promo.js
+++ movies-explorer/src/components/Main/Promo/Promo.js
@@ -3,10 +3,10 @@ import './Promo.css';
 
 function Promo() {
     return (
-        <div className="promo">
+        <section className="promo">
             <h1 className='promo__header'>Учебный проект студента факультета Веб-разработки.</h1>
             <NavTab className='promo__nav-tab' />
-        </div>
+        </section>
     );
 }
 
