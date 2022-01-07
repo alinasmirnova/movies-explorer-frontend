6dc5a3c6d47c45cc4f64bc2aa54e488e1dd75afc
Alina Smirnova <BlueOstrich@yandex.ru>
2022-01-07 16:35:29 +0300

    feat: вёрстка блока технологии (неправильные отсутпы)

--- movies-explorer/src/components/Main/Techs/Techs.js
+++ movies-explorer/src/components/Main/Techs/Techs.js
@@ -1,8 +1,26 @@
+import SectionTitle from '../SectionTitle';
+import List from '../../List';
+import './Techs.css';
+
 function Techs() {
     return (
-        <div className="techs">
-
-        </div>
+        <section className="techs">
+            <SectionTitle>Технологии</SectionTitle>
+            <div className='techs__body'>
+                <h2 className="techs__header">7 технологий</h2>
+                <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+                <List className="techs__list">
+                    <span className="techs__list-item">HTML</span>
+                    <span className="techs__list-item">CSS</span>
+                    <span className="techs__list-item">JS</span>
+                    <span className="techs__list-item">React</span>
+                    <span className="techs__list-item">Git</span>
+                    <span className="techs__list-item">Express.js</span>
+                    <span className="techs__list-item">mongoDB</span>
+                </List>
+            </div>
+            
+        </section>
     );
 }
 
