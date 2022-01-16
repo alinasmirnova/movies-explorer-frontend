4ddd449b7b813594d4dd41234bc3cf368e747686
Alina Smirnova <BlueOstrich@yandex.ru>
2022-01-16 14:20:54 +0300

    feat: страница профиля

--- movies-explorer/src/components/App/App.js
+++ movies-explorer/src/components/App/App.js
@@ -3,9 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import Main from '../Main';
 import Movies from '../Movies';
 import SavedMovies from '../SavedMovies';
+import Profile from '../Profile/Profile';
 import './App.css';
 
 function App() {
+    const handleLogout = () => {
+
+    }
+    
     return (
         <div className="body">
             <div className="body__container">
@@ -13,6 +18,7 @@ function App() {
                     <Route exact path="/" element={<Main />} />
                     <Route exact path="/movies" element={<Movies />} />
                     <Route exact path="/saved-movies" element={<SavedMovies />} />
+                    <Route exact path="/profile" element={<Profile onLogout={handleLogout} />} />
                 </Routes>
             </div>            
         </div>
