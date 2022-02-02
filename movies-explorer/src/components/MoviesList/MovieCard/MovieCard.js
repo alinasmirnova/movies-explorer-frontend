3ccf51af21592268aed1ec0290afdddddb01f69b
Alina Smirnova <alinasmirnova915@gmail.com>
2022-02-02 07:22:55 +0300

    feat: откывать трейлер по клику на изображение

--- movies-explorer/src/components/MoviesList/MovieCard/MovieCard.js
+++ movies-explorer/src/components/MoviesList/MovieCard/MovieCard.js
@@ -4,6 +4,7 @@ import likeIcon from '../../../images/like.svg';
 import likeDisabledIcon from '../../../images/like-disabled.svg';
 import deleteIcon from '../../../images/delete.svg';
 import './MovieCard.css';
+import ExternalLink from '../../ExternalLink';
 
 function MovieCard({ card, onLike, onDelete }) {
     const onClick = (e) => {
@@ -17,7 +18,9 @@ function MovieCard({ card, onLike, onDelete }) {
 
     return (
         <div className="card">
-            <img className="card__thumbnail" src={card.thumbnail} alt={card.nameRU} />
+            <ExternalLink to={card.trailer}>
+                <img className="card__thumbnail" src={card.thumbnail} alt={card.nameRU} />
+            </ExternalLink>
             <h2 className="card__title">{card.nameRU}</h2>
             <Button className="card__action" onClick={onClick}>
                 { onLike && <img className="card__like-icon" src={card.isSaved ? likeIcon : likeDisabledIcon} alt={ card.isSaved ? "Удалить" : "Сохранить"}/> }
